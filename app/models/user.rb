b25eac34d13a7be7c7d4bce1bf8d66a9335843e3
Adam Wells <adamwells.dev@gmail.com>
2015-04-07 11:17:55 -0700

    add users via form

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,25 @@ class User < ActiveRecord::Base
 	validates_presence_of :username, :email, :session_token, :password_digest
 	validates_uniqueness_of :username, :email, :session_token
 
-	
+	after_initialize :ensure_session_token
+
+	def password=(password)
+		self.password_digest = BCrypt::Password.create(password)
+	end
+
+	def self.generate_session_token
+		SecureRandom.urlsafe_base64
+	end
+
+	def reset_session_token
+		self.session_token = User.generate_session_token
+		self.save!
+		self.session_token
+	end
+
+	private
+
+	def ensure_session_token
+		self.session_token ||= User.generate_session_token
+	end
 end
