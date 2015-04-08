045fed29e7d4eafbf1e1217a75a9031fd426084b
Adam Wells <adamwells.dev@gmail.com>
2015-04-07 21:32:24 -0700

    add photo seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,11 @@
 User.destroy_all()
 Profile.destroy_all()
 
+PHOTOS = ['http://i.imgur.com/sciSQG3.jpg', 'http://i.imgur.com/BhqqCZG.jpg', 'http://i.imgur.com/FL1hyX8.jpg',
+'http://i.imgur.com/OcrRV00.jpg', 'http://i.imgur.com/Fed3Qmu.jpg', 'http://i.imgur.com/BUPVDoX.jpg',
+'http://i.imgur.com/PPAArko.jpg', 'http://i.imgur.com/uU2oxH1.jpg', 'http://i.imgur.com/TkdCEZB.jpg',
+'http://i.imgur.com/SdCtKB8.jpg']
+
 User.create!(email: 'adam', username: 'adam', password: 'adam')
 
 20.times do
@@ -17,8 +22,9 @@ User.create!(email: 'adam', username: 'adam', password: 'adam')
 end
 
 21.times do |i|
-	Profile.create!(user_id: i + 1,
+	Profile.create!(user_id: User.all[i].id,
 									self_summary: Faker::Lorem.paragraph,
 									good_at: Faker::Lorem.paragraph,
-									message_if: Faker::Lorem.paragraph)
+									message_if: Faker::Lorem.paragraph,
+									photo_url: PHOTOS.sample)
 end
