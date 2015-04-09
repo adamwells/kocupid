b9e5490b74e267b41fc3905d492cef13639a33bf
Adam Wells <adamwells.dev@gmail.com>
2015-04-08 21:43:37 -0700

    add basic inbox view

--- app/assets/javascripts/views/messages/index.js
+++ app/assets/javascripts/views/messages/index.js
@@ -5,21 +5,31 @@ Kocupid.Views.MessagesIndex = Backbone.CompositeView.extend({
 		this.listenTo(this.collection, 'sync', this.render);
 	},
 
-	addMessage: function (message) {
+	addMessage: function (selector, message) {
 		var indexItem = new Kocupid.Views.MessagesIndexItem({ model: message });
-		this.addSubview('.messages-index', indexItem);
+		this.addSubview(selector, indexItem);
 	},
 
-	addMessages: function () {
-		this.collection.each(function (message) {
-			this.addMessage(message);
+	sentMessages: function () {
+		sent = this.collection.where({ sender_id: Kocupid.currentUserId });
+		return new Kocupid.Collections.Messages(sent);
+	},
+
+	receivedMessages: function () {
+		received = this.collection.where({ recipient_id: Kocupid.currentUserId });
+		return new Kocupid.Collections.Messages(received);
+	},
+
+	addMessages: function (selector, collection) {
+		collection.each(function (message) {
+			this.addMessage(selector, message);
 		}.bind(this));
 	},
 
 	render: function () {
 		var content = this.template();
 		this.$el.html(content);
-		this.addMessages();
+		this.addMessages('.sent-messages-index', this.sentMessages());
 		return this;
 	}
 });
--- app/views/api/messages/index.json.jbuilder
+++ app/views/api/messages/index.json.jbuilder
@@ -1,5 +1,7 @@
 json.array! @messages do |message|
-	json.sender message.sender.username
-	json.recipient message.recipient.username
+	json.sender_username message.sender.username
+	json.recipient_username message.recipient.username
+	json.sender_id message.sender.id
+	json.recipient_id message.recipient.id
 	json.body message.body
 end
